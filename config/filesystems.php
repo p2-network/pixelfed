2900477eb481909b2ea561140855c8a9a667679b
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2025-01-25 21:16:29 +1100

    s3 uploads are private again (3)

--- config/filesystems.php
+++ config/filesystems.php
@@ -73,7 +73,7 @@ return [
             'secret'   => env('AWS_SECRET_ACCESS_KEY'),
             'region'   => env('AWS_DEFAULT_REGION'),
             'bucket'   => env('AWS_BUCKET'),
-            'visibility' => env('AWS_VISIBILITY', 'public'),
+            'visibility' => env('AWS_VISIBILITY', 'private'),
             'url'      => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
